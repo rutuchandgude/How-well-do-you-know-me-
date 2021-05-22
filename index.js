428843bec100258d0a0a61427c53b2e4ab7119b0
rutuchandgude <74643325+rutuchandgude@users.noreply.github.com>
2021-05-22 23:19:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+var readlineSync=require("readline-sync");
+var score=0;
+var userName=readlineSync.question("What is your name? ");
+console.log("Welcome "+ userName +" to DO YOU KNOW Rutuja?");
+//play function
+function play(question,answer)
+{
+  var userAnswer=readlineSync.question(question);
+
+if(userAnswer===answer)
+{
+  console.log("right!");
+  score=score+1;
+}
+else
+{
+  console.log("Wrong!");
+}
+console.log("current score: ",score);
+console.log("-----------");
+}
+//Array of the objects
+var questions=[{
+  question:"Where do I live? ",
+  answer:"Kopargaon"
+},
+{
+  question:"My fevorite superhero would be?",
+  answer:"Hardik"
+}]
+//loop
+for(var i=0;i<questions.length;i++)
+{
+  var currentQuestion=questions[i];
+  play(currentQuestion.question,currentQuestion.answer)
+}
+console.log("YAY! You SCORED:",score);
